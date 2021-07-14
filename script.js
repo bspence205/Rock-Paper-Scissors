120219de15c155ec209a952d9b11cb725746fc22
bspence205 <77554087+bspence205@users.noreply.github.com>
2021-07-13 22:43:20 -0700

    partial component for single round game

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 // component to randomly generate computers choice
 
 function computerPlay() {
-  choice = Math.floor(Math.random() * 4);
+  let choice = Math.floor(Math.random() * 4);
   if (choice == 1) {
     console.log("Rock");
   } else if (choice == 2) {
     console.log("Paper");
   } else if (choice == 3) {
-    console.log("Scizzor");
+    console.log("Scissor");
   }
 }
-computerPlay();
+
+// partial component for single round game
+
+const playerSection = "paper";
+const computerSelection = computerPlay();
+function singleRound(playerSection, computerSelection) {
+  if (playerSection == "rock") {
+    return "You Lose";
+  } else if (playerSection == "scissor") {
+    return "You Win";
+  }
+}
+
+console.log(singleRound(playerSection, computerSelection));
