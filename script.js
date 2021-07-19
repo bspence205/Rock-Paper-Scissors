72cdb5fd5666fb1df5831697f236999f7ff959b9
bspence205 <77554087+bspence205@users.noreply.github.com>
2021-07-19 01:10:21 -0700

    playround component; need to refactor old code

--- script.js
+++ script.js
@@ -21,16 +21,63 @@ function computerPlay() {
 
 // other component for single round
 
-function pRound(pSelection) {
+function playRound(playerSelection) {
   let compSelection = compPlay();
   let result = "";
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")
+  ) {
+    playerScore += 1;
+    result =
+      "You Won! " +
+      playerSelection +
+      " beats " +
+      computerSelection +
+      "<br><br>Player score: " +
+      playerScore +
+      "<br>Computer score: " +
+      computerScore;
+
+    if (playerScore == 5) {
+      result += "<br><br>You won the game! Reload the page to play again";
+    }
+  } else if (playerSelection == computerSelection) {
+    result =
+      "It's a draw. You both chose " +
+      playerSelection +
+      "<br><br>Player score: " +
+      playerScore +
+      "<br>Computer score: " +
+      computerScore;
+  } else {
+    computerScore += 1;
+    result =
+      "You lost! " +
+      computerSelection +
+      " beats " +
+      playerSelection +
+      "<br><br>Player score: " +
+      playerScore +
+      "<br>Computer score: " +
+      computerScore;
+
+    if (computerScore == 5) {
+      result += "<br><br>I won the game! Reload the page and try harder";
+      disableButtons();
+    }
+  }
+
+  document.getElementById("result").innerHTML = result;
+  return;
 }
 
 // component for single round game
 const playerSelection = ["Rock", "Paper", "Scissor"];
 const computerSelection = computerPlay();
 
-function playRound(playerChoice, computerSelection) {
+function pRound(playerChoice, computerSelection) {
   if (computerSelection == 0) {
     return "Draw, Rock respects Rock";
   } else if (computerSelection == 2) {
